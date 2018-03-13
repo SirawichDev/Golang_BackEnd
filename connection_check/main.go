d4e9c8c6ec4d9e8f422dc7a14caa95ce659cd8bf
SirawichDev <sirawit0676@gmail.com>
2018-03-13 07:50:36 +0700

    connection check added

---
+++ connection_check/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+
+		}
+
+		io.WriteString(conn, "I see you connected.")
+		conn.Close()
+	}
+}
