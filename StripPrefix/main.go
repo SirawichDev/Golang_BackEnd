107d667bec5078fa757ef6cf7d1045c159d91eb0
SirawichDev <sirawit0676@gmail.com>
2018-03-14 23:07:36 +0700

    bug not fix yet

--- StripPrefix/main.go
+++ StripPrefix/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"io"
 	"net/http"
-	
 )
-func main(){
 
-	http.HandleFunc("/",quote)
-	http.Handle("/assets/", http.StripPrefix("/assets",http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe(":8080",nil)
+func main() {
+
+	http.HandleFunc("/", quote)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8080", nil)
 }
 
-func quote(res http.ResponseWriter,req)
+func quote(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(res,"`<img src="/assets/tt.png">`)
+}
