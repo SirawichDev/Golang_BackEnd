7a7e83c8cbdc349f6f29fe6af6784ad683fa8586
SirawichDev <sirawit0676@gmail.com>
2018-03-14 11:10:14 +0700

    move init() insert in p2

--- passDatastruct/main.go
+++ passDatastruct/main.go
@@ -8,13 +8,17 @@ import (
 
 var tpl *template.Template
 
-func init() {
-	tpl = template.Must(template.ParseFiles("show.html"))
-}
+//func init() {
+//	tpl = template.Must(template.ParseFiles("show.html"))
+//}
 func p2(res http.ResponseWriter, req *http.Request) {
 	sage := []string{"hh", "vv", "ee", "bb"}
-	err := tpl.ExecuteTemplate(res, "show.html", sage)
+	temp, err := template.ParseFiles("show.html")
 	if err != nil {
+		log.Fatalln(err)
+	}
+	show := temp.ExecuteTemplate(res, "show.html", sage)
+	if show != nil {
 		log.Fatalln(err)
 	}
 }
