ae47bbf84fce59c5583744a0536bd956e02cfb69
SirawichDev <sirawit0676@gmail.com>
2018-03-14 10:31:02 +0700

    pass value on handle func

--- HandleFunc/main.go
+++ HandleFunc/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -13,11 +16,24 @@ func p1(res http.ResponseWriter, req *http.Request) {
 func p2(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "you're at page2")
 }
+func p3(res http.ResponseWriter, req *http.Request) {
+	fi, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = fi.ExecuteTemplate(res, "index.html", "20")
+	if err != nil {
+		fmt.Println(nil)
+		log.Fatalln(err)
 
+	}
+}
 func main() {
 
 	http.HandleFunc("/p1/", p1)
 	http.HandleFunc("/p2", p2)
-
+	http.Handle("/p3/", http.HandlerFunc(p3))
+	fmt.Println(nil)
 	http.ListenAndServe(":9999", nil)
+
 }
