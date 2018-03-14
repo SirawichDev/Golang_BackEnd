79e0b5c34bcf2ec45cefe59fec2ad39b766384dd
SirawichDev <sirawit0676@gmail.com>
2018-03-14 22:16:09 +0700

    added p3 in fileserve

--- file_sever/main.go
+++ file_sever/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/qoute/1", p1)
+	http.HandleFunc("/p2", p1)
+	http.HandleFunc("/p3", p3)
 	http.ListenAndServe(":8080", nil)
 }
 
 func p1(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, `<img src="q1.jpg">`)
+}
+func p3(res http.ResponseWriter, req *http.Request) {
+	tpl
 }
